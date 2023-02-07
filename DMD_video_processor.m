8f983bac9d6b7b93da317bdd0490baf3324e7240
SamirVK <samir.karam@mail.concordia.ca>
2023-02-06 21:16:44 -0500

    Added back/foreground separation and adjusted plotting features

--- DMD_video_processor.m
+++ DMD_video_processor.m
@@ -21,22 +21,19 @@ clear colorVidFrames
 whos gFrames
 
 %% Print a grayscale frame from the gFrames matrix
-toPrint = reshape(gFrames(:,1),[height,width]); 
-figure
-imshow(toPrint)
+% toPrint = reshape(gFrames(:,1),[height,width]); 
+% figure
+% imshow(toPrint)
 
-%% GENERALIZED Low-rank SVD (CHOOSE to be less than or equal to 95)
+%% GENERALIZED Low-rank SVD (CHOOSE between 1 and 100)
 % We want to find the DMD matrix A that satisfies the argmin problem. 
 % This involves computing the Moore-Penrose pseudoinverse X_dagger and 
 % multiplying on the left by the shifted matrix Y whose columns are the 
 % next time step from those in the X matrix.
 
-s = 90; % Desired energy level%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+s = 100; % Desired energy level %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 gFrames = double(gFrames);
-% X = gFrames(:,1:end-1);
-% Y = gFrames(:,2:end);
-% X = double(X);
-% Y = double(Y);
+
 [U,S,V] = svd(gFrames,'econ');
 singular_values = diag(S);
 total_energy = sum(diag(S));
@@ -45,7 +42,7 @@ i = 1;
 while 1
     cum_energy = cum_energy + singular_values(i);
     percent_energy = cum_energy / total_energy * 100;
-    if percent_energy >= s
+    if percent_energy > s
         break
     end
     i = i + 1;
@@ -58,7 +55,29 @@ X_dagger = pinv(X);
 A = Y * X_dagger;
 
 %% Get the e'values and e'vectors of A
-[eV, D] = eig(A);
+[eVectors, eValues] = eig(A);
+mu = diag(eValues);
+
+%% Plotting Discrete E'values (Code reused from Jason Bramburger's DMD.m)
+
+% % make axis lines
+% line = -15:15;
+% 
+% % Unit circle
+% th = linspace(0,2*pi,1000);
+% xcos = cos(th);
+% ysin = sin(th);
+% 
+% figure(1)
+% plot(zeros(length(line),1),line,'k','Linewidth',2) % imaginary axis
+% hold on 
+% plot(line,zeros(length(line),1),'k','Linewidth',2) % real axis
+% plot(xcos,ysin,'k--','LineWidth',2) % unit circle
+% plot(mu,'.','Color',[1 69/255 79/255],'Markersize',30)
+% hold off
+% xlabel('Re(\mu)')
+% ylabel('Im(\mu)')
+% set(gca,'FontSize',16,'Xlim',[-1.2 1.2],'Ylim',[-1.2 1.2])
 
 %% Build the DMD video matrix
 Z = zeros(i,frames);
@@ -69,19 +88,95 @@ for n = 1:frames-1
     Z(:,n+1) = A*Z(:,n); 
 end
 
-%% Compare end norms
-norm(Y(:,end) - Z(:,end))
+%% Construct the "low-rank" DMD frames
+% Solve the linear system given by the eigenbasis expansion
+c = eVectors \ X(:,1);
+dt = 1;
 
-%% make a movie from the DMD video matrix frames:
-% Recover original dimensionality
-movie = U(:,1:i) * Z;
+% Determine the indices of the low-rank modes 
+modes_low = zeros(i,1);
+for j = 1:i
+    if abs(mu(j)) <= 1
+        modes_low(j) = 1;
+    end 
+end 
 
-toPrint = reshape(movie,[height,width,379]);
-figure
-f = 1;
-while f <= frames
-    imshow(mat2gray(toPrint(:,:,f),[0,255]))
-    pause(1/120);
-    f = f + 1;
+% Construct the x matrix, which is a simple sum of the modes 
+x = zeros(i,frames);
+for t = 1:frames
+    for k = 1:i
+        x(:,t) = x(:,t) + c(k) * eVectors(:, k) * mu(k)^((t-1)/dt);
+    end 
 end
+
+% Construct the X_lowrank DMD matrix for the low-rank modes
+x_low = zeros(i,frames);
+for t = 1:frames
+    for k = 1:i
+        if modes_low(k) == 1
+            x(:,t) = x(:,t) + c(k) * eVectors(:, k) * mu(k)^((t-1)/dt);
+        end
+    end 
+end
+
+%% Construct the sparse DMD frames and fix negative values for both the
+% sparse and low-rank DMD matrices
+
+x_sparse = x - abs(x_low);
+
+% Put back the negative entries from x_sparse into x_low
+negatives = x_sparse;
+for t = 1:frames
+    for j = 1:i
+        if x_sparse(j,t) < 0
+            negatives(j,t) = 0;
+        end
+    end
+end
+x_low = negatives + abs(x_low);
+
+% Then subtract them from x_sparse
+x_sparse = x_sparse - negatives;
+
+%% Print frames from matrices
+subplot(2,2,1)
+snapshot = U(:,1:i) * X;
+toPrint = reshape(snapshot,[height,width,frames-1]);
+imshow(mat2gray(toPrint(:,:,342),[0,255]))
+title('Original')
+
+subplot(2,2,2)
+x_real = real(x);
+snapshot = U(:,1:i) * x_real;
+toPrint = reshape(snapshot,[height,width,frames]);
+imshow(mat2gray(toPrint(:,:,342),[0,255]))
+title('Reconstructed')
+
+subplot(2,2,3)
+x_low_real = real(x_low);
+snapshot = U(:,1:i) * x_low_real;
+toPrint = reshape(snapshot,[height,width,frames]);
+imshow(mat2gray(toPrint(:,:,342),[0,255]))
+title('Foreground')
+
+subplot(2,2,4)
+x_sparse_real = real(x_sparse);
+snapshot = U(:,1:i) * x_sparse_real;
+toPrint = reshape(snapshot,[height,width,frames]);
+imshow(mat2gray(toPrint(:,:,342),[0,255]))
+title('Background')
+
+%% Make a movie from the DMD video matrix frames:
+% Recover original dimensionality
+
+% movie = U(:,1:i) * Z;
+% 
+% toPrint = reshape(movie,[height,width,frames]);
+% figure(2)
+% f = 1;
+% while f <= frames
+%     imshow(mat2gray(toPrint(:,:,f),[0,255]))
+%     pause(1/120);
+%     f = f + 1;
+% end
 
